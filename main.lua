dcf8917f518e0a7d227465cb4213d0d49cbab9ec
sracabaxi <sir.acabaxi@gmail.com>
2026-03-20 14:38:07 -0300

    codigo refatorizado

--- main.lua
+++ main.lua
@@ -2,8 +2,8 @@
 function love.load()
 
 	rect = {
-		size = {x = 100, y = 100, r = 10},
-		positionx = {f = 100, s = 250, t = 400},
+		size = {100, 10},
+		positionx = {100, 250, 400},
 		positiony = 100}
 
 end
@@ -12,8 +12,8 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.rectangle("line", rect.positionx.f, rect.positiony, rect.size.x, rect.size.y, rect.size.r)
-	love.graphics.rectangle("line", rect.positionx.s, rect.positiony, rect.size.x, rect.size.y, rect.size.r)
-	love.graphics.rectangle("line", rect.positionx.t, rect.positiony, rect.size.x, rect.size.y, rect.size.r)
+	love.graphics.rectangle("line", rect.positionx[1], rect.positiony, rect.size[1], rect.size[1], rect.size[2])
+	love.graphics.rectangle("line", rect.positionx[2], rect.positiony, rect.size[1], rect.size[1], rect.size[2])
+	love.graphics.rectangle("line", rect.positionx[3], rect.positiony, rect.size[1], rect.size[1], rect.size[2])
 	love.graphics.setBackgroundColor(226/255, 170/255, 255/255, 1)
 end

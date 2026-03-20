22f4b16278850af3d1c2e997e3e1c5f2695426d6
sracabaxi <sir.acabaxi@gmail.com>
2026-03-20 16:36:20 -0300

    mostrar progresso

--- main.lua
+++ main.lua
@@ -7,7 +7,10 @@ function love.load()
 		--[[250, 
 		400},]]}
 
-	cells = {"X", "_", "O"}
+	cells = {
+		Top = {"X", "_", "O"},
+		Mid = {"X", "O", "_"},
+		Bot = {"X", "O", "_"}}
 end
 
 function love.update(dt)
@@ -18,12 +21,16 @@ function love.draw()
 	--[[love.graphics.rectangle("line", rect[2][2], rect[3], rect[1][1], rect[1][1], rect[1][2])
 	love.graphics.rectangle("line", rect[2][3], rect[3], rect[1][1], rect[1][1], rect[1][2])]]
 	for i = 1, 3 do
-		if i == 1 then
-			love.graphics.print(cells[i], 0, 0)
-		else
-			local I = i - 1
-			love.graphics.print(cells[i], I * 25, 0)
-		end
+		local I = i - 1
+		love.graphics.print(cells.Top[i], I * 25, 0)
+	end
+	for i = 1, 3 do
+		local I = i - 1
+		love.graphics.print(cells.Mid[i], I * 25, 15)
+	end
+	for i = 1, 3 do
+		local I = i - 1
+		love.graphics.print(cells.Bot[i], I * 25, 30)
 	end
 	love.graphics.setBackgroundColor(226/255, 170/255, 255/255, 1)
 end

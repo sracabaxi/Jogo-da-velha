394e54f1bae81da36c457b9c2290ef7590f4e0df
sracabaxi <sir.acabaxi@gmail.com>
2026-03-20 15:00:23 -0300

    codigo refatorizado (apenas um retangulo)

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 
 function love.load()
 
-	rect = {{100, 10}, {100, 250, 400},100}
+	rect = {
+		{100, 10},
+		{100, 
+		--[[250, 
+		400]]},
+		100}
 
 end
 
@@ -10,7 +15,7 @@ end
 
 function love.draw()
 	love.graphics.rectangle("line", rect[2][1], rect[3], rect[1][1], rect[1][1], rect[1][2])
-	love.graphics.rectangle("line", rect[2][2], rect[3], rect[1][1], rect[1][1], rect[1][2])
-	love.graphics.rectangle("line", rect[2][3], rect[3], rect[1][1], rect[1][1], rect[1][2])
+	--[[love.graphics.rectangle("line", rect[2][2], rect[3], rect[1][1], rect[1][1], rect[1][2])
+	love.graphics.rectangle("line", rect[2][3], rect[3], rect[1][1], rect[1][1], rect[1][2])]]
 	love.graphics.setBackgroundColor(226/255, 170/255, 255/255, 1)
 end

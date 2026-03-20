c8f1e858230a16e4c89955e4da9677f9a555397d
sracabaxi <sir.acabaxi@gmail.com>
2026-03-20 19:44:13 -0300

    9 posicoes

--- main.lua
+++ main.lua
@@ -3,9 +3,10 @@ function love.load()
 
 	rect = {
 		size = {xy = 100, r = 10},
-		position = 100--, 
-		--[[250, 
-		400},]]}
+		positionx = {100, 
+		250, 
+		400},
+		positiony = {100, 250, 400}}
 
 	cells = {
 		{"X", "_", "O"},
@@ -18,7 +19,7 @@ end
 
 function love.draw()
 	love.graphics.setBackgroundColor(226/255, 170/255, 255/255, 1)
-	love.graphics.rectangle("line", rect.position, rect.position, rect.size.xy, rect.size.xy, rect.size.r)
+	--love.graphics.rectangle("line", rect.position, rect.position, rect.size.xy, rect.size.xy, rect.size.r)
 	--[[love.graphics.rectangle("line", rect[2][2], rect[3], rect[1][1], rect[1][1], rect[1][2])
 	love.graphics.rectangle("line", rect[2][3], rect[3], rect[1][1], rect[1][1], rect[1][2])]]
 	--[[for i = 1, 3 do
@@ -32,12 +33,17 @@ function love.draw()
 	for i = 1, 3 do
 		local I = i - 1
 		love.graphics.print(cells.Bot[i], I * 25, 30)
-	end]]
+	end
 	for y = 1, 3 do
 		for x = 1, 3 do
 			local X = x - 1
 			local Y = y - 1
-			love.graphics.print(cells[y][x], X * 25, Y * 25)
+			--love.graphics.print(cells[y][x], X * 25, Y * 25)
+		end
+	end]]
+	for positionY = 1, 3 do
+		for positionX = 1, 3 do
+			love.graphics.rectangle("line", rect.positionx[positionX], rect.positiony[positionY], rect.size.xy, rect.size.xy, rect.size.r)
 		end
 	end
 end

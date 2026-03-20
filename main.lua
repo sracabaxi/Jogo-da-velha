e1d25255a2965d1de30ba9b7f845023bbc49f489
sracabaxi <sir.acabaxi@gmail.com>
2026-03-20 15:12:42 -0300

    cod refatorizado mas com objs

--- main.lua
+++ main.lua
@@ -2,11 +2,10 @@
 function love.load()
 
 	rect = {
-		{100, 10},
-		{100, 
+		size = {xy = 100, r = 10},
+		position = 100--, 
 		--[[250, 
-		400]]},
-		100}
+		400},]]}
 
 end
 
@@ -14,7 +13,7 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.rectangle("line", rect[2][1], rect[3], rect[1][1], rect[1][1], rect[1][2])
+	love.graphics.rectangle("line", rect.position, rect.position, rect.size.xy, rect.size.xy, rect.size.r)
 	--[[love.graphics.rectangle("line", rect[2][2], rect[3], rect[1][1], rect[1][1], rect[1][2])
 	love.graphics.rectangle("line", rect[2][3], rect[3], rect[1][1], rect[1][1], rect[1][2])]]
 	love.graphics.setBackgroundColor(226/255, 170/255, 255/255, 1)

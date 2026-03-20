06eeaf92f6ff56e73ff906f99ace34feeae8cae9
sracabaxi <sir.acabaxi@gmail.com>
2026-03-20 15:32:03 -0300

    mostrar como está

--- main.lua
+++ main.lua
@@ -17,6 +17,8 @@ function love.draw()
 	love.graphics.rectangle("line", rect.position, rect.position, rect.size.xy, rect.size.xy, rect.size.r)
 	--[[love.graphics.rectangle("line", rect[2][2], rect[3], rect[1][1], rect[1][1], rect[1][2])
 	love.graphics.rectangle("line", rect[2][3], rect[3], rect[1][1], rect[1][1], rect[1][2])]]
-	love.graphics.print(cells, 0, 0)
+	love.graphics.print(cells[1], 0, 0)
+	love.graphics.print(cells[2], 0, 0)
+	love.graphics.print(cells[3], 0, 0)
 	love.graphics.setBackgroundColor(226/255, 170/255, 255/255, 1)
 end

5a2b1d1d4c0566a69db503dceac7c4ac66c4cb58
sracabaxi <sir.acabaxi@gmail.com>
2026-03-21 16:43:34 -0300

    refazer mudanças

--- main.lua
+++ main.lua
@@ -1,12 +1,8 @@
 
 function love.load()
-	
-	display = { 
-		width = love.graphics.getWidth(),
-		height = love.graphics.getHeight()}
-	
+
 	rect = {
-		size = {xy = display.height / 3, r = 10},
+		size = {xy = 100, r = 10},
 		padding = 0,
 		margin = {x = 0, y = 0}}
 		
@@ -33,11 +29,9 @@ function love.draw()
 	love.graphics.setBackgroundColor(226/255, 170/255, 255/255, 1)
 	for positionY = 1, 3 do
 		for positionX = 1, 3 do
-			local xsize = rect.size.xy * 3 + rect.padding * 2
-			local middle = positionX * (display.width / 2 - xsize / 2)
-			--local X = (positionX - 1) * (rect.size.xy + rect.padding) + rect.margin.x 
-			local Y = (positionY - 1) * (rect.size.xy + rect.padding) + rect.margin.y
-			love.graphics.rectangle("line", middle, Y, rect.size.xy, rect.size.xy, rect.size.r)
+				X = (positionX - 1) * (rect.size.xy + rect.padding) + rect.margin.x
+				Y = (positionY - 1) * (rect.size.xy + rect.padding) + rect.margin.y
+			love.graphics.rectangle("line", X, Y, rect.size.xy, rect.size.xy, rect.size.r)
 		end
 	end
 	for y = 1, 3 do
